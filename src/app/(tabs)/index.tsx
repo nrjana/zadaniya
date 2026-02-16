dbe24fb3d4ef4dfb62554482c0100935b6e5c6e9
nrjana <nurjantoktogulova13@gmail.com>
2026-02-16 14:05:01 +0600

    1 zd

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,46 @@
-import Greeting from "@/components/Greeting";
-import { View } from "react-native";
+import { Alert, Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function HomeScreen() {
-  return <View style={{ flex: 1, backgroundColor: "#fff" }}>
-    <Greeting name="Nasirdin" role="teacher"/>
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Имя студента: Min Yoongi</Text>
+      <Text style={styles.subtitle}>Название курса: K-pop rap lessons</Text>
 
-
-    <ProfileCard name="student_name" role="student_role" course="course_name" bio="student_info"/>
- 
-  </View>;
+      <Pressable 
+        style={styles.button}
+        onPress={() => Alert.alert("Кнопка работает!")}
+      >
+        <Text style={styles.buttonText}>click here</Text>
+      </Pressable>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  subtitle: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#800080",
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
